141cb3f491b5941c9eb9bfbbe4e1dbc1ef5841a9
three-i <lntelligence273@gmail.com>
2021-03-02 17:37:32 +0600

    Three_i Portfolio

---
+++ assets/php/contact-control.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$name = $_POST['name'];
+$vistor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_form = $vistor_email;
+
+$email_subject = $_POST['subject'];
+
+$email_body = "Username: $name.\n".
+                "User Email: $vistor_email.\n".
+                    "User Message: $message.\n";
+
+$to = "lntelligence273@gmail.com";
+
+$headers = "From: $email_form.\r\n";
+$headers .= "Reply-to: $vistor_email.\r\n";
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: ../../contact-me.html");
+
+?>
