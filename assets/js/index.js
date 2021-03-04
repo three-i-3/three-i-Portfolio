27f45af8e4ecbc4e9bb7daeaeb887b0a5767408f
three-i <lntelligence273@gmail.com>
2021-03-04 11:36:15 +0600

    three_i

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,15 @@ const container = document.querySelector(".container");
 humburger_menu.addEventListener("click", ()=> {
     container.classList.toggle("active");
 })
+
+Email.send({
+    Host : "smtp.gmail.com",
+    Username : "lntelligence273@gmail.com",
+    Password : "password",
+    To : 'lntelligence273@gmail.com',
+    From : document.getElementById('ConEmail'),
+    Subject : document.getElementById('conSubj'),
+    Body : document.getElementById('conSMS')
+}).then(
+  message => alert(message)
+);
