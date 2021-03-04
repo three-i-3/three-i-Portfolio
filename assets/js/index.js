9ab1b68658fbee57d2a0ed904e04252e612f2180
three-i <lntelligence273@gmail.com>
2021-03-04 12:52:18 +0600

    three_i

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,22 @@
 const humburger_menu = document.querySelector(".humburger-menu");
 const container = document.querySelector(".container");
 
-humburger_menu.addEventListener("click", ()=> {
-    container.classList.toggle("active");
+humburger_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
 })
 
 Email.send({
-    Host : "smtp.gmail.com",
-    Username : "lntelligence273@gmail.com",
-    Password : "password",
-    To : 'lntelligence273@gmail.com',
-    From : document.getElementById('ConEmail'),
-    Subject : document.getElementById('conSubj'),
-    Body : document.getElementById('conSMS')
+  Host: "smtp.gmail.com",
+  Username: "lntelligence273@gmail.com",
+  Password: "password",
+  To: 'lntelligence273@gmail.com',
+  From: document.getElementById('ConEmail'),
+  Subject: document.getElementById('conSubj'),
+  Body: document.getElementById('conSMS')
 }).then(
   message => alert(message)
 );
+
+// Progress Bar Javascript
+
+new WOW().init();
