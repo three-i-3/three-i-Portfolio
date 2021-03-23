f7d3233fa402ab5b359ce41f39ab435fe59bcbd7
three-i <lntelligence273@gmail.com>
2021-03-23 21:38:25 +0600

    Three_i | Portfolio

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,17 +5,7 @@ humburger_menu.addEventListener("click", () => {
   container.classList.toggle("active");
 })
 
-Email.send({
-  Host: "smtp.gmail.com",
-  Username: "lntelligence273@gmail.com",
-  Password: "password",
-  To: 'lntelligence273@gmail.com',
-  From: document.getElementById('ConEmail'),
-  Subject: document.getElementById('conSubj'),
-  Body: document.getElementById('conSMS')
-}).then(
-  message => alert(message)
-);
+
 
 // Progress Bar Javascript
 
